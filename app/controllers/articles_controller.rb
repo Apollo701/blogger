24b92694a121314ce3b5020182f870616afb8ce4
Jason Dorn <apollo701@gmail.com>
2015-11-05 00:52:43 -0800

    Finished form workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,46 @@ def show
 	@article = Article.find(params[:id])
 end
 
+def new
+	@article = Article.new
+end
+
+def create
+	@article = Article.new(article_params)
+	if @article.save
+		redirect_to article_path(@article)
+	else
+		redirect_to new_article_path
+	end
+end
+
+def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	redirect_to articles_path
+end
+
+def edit
+	@article = Article.find(params[:id])
+end
+
+def update
+	@article = Article.find(params[:id])
+	if @article.update(article_params)
+		flash.notice = "Article '#{@article.title}' Updated!"
+		redirect_to article_path(@article)
+	else
+		redirect_to edit_article_path(@article)
+	end
+end
+
+
+
+	private
+
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
 end
