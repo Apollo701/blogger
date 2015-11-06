1c135087fff3604065d99298defc8f0a73f931dd
Jason Dorn <apollo701@gmail.com>
2015-11-06 00:11:15 -0800

    Taggign feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,56 +1,47 @@
 class ArticlesController < ApplicationController
-
-def index
-	@articles = Article.all
-end
-
-def show
-	@article = Article.find(params[:id])
+	include ArticlesHelper
 	
-	@comment = Comment.new
-	@comment.article_id = @article.id
-end
-
-def new
-	@article = Article.new
-end
-
-def create
-	@article = Article.new(article_params)
-	if @article.save
-		redirect_to article_path(@article)
-	else
-		redirect_to new_article_path
-	end
-end
-
-def destroy
-	@article = Article.find(params[:id])
-	@article.destroy
-	redirect_to articles_path
-end
-
-def edit
-	@article = Article.find(params[:id])
-end
-
-def update
-	@article = Article.find(params[:id])
-	if @article.update(article_params)
-		flash.notice = "Article '#{@article.title}' Updated!"
-		redirect_to article_path(@article)
-	else
-		redirect_to edit_article_path(@article)
-	end
-end
-
-
-
-	private
-
-
-	def article_params
-		params.require(:article).permit(:title, :body)
+	def index
+		@articles = Article.all
 	end
 
+	def show
+		@article = Article.find(params[:id])
+		
+		@comment = Comment.new
+		@comment.article_id = @article.id
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to article_path(@article)
+		else
+			redirect_to new_article_path
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash.notice = "Article '#{@article.title}' Updated!"
+			redirect_to article_path(@article)
+		else
+			redirect_to edit_article_path(@article)
+		end
+	end
 end
